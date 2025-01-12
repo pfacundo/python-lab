12aba28c01409504a36b7c6016da6ac0190c354b
facundo.p <facundo.huira@gmail.com>
2025-01-12 01:37:37 -0300

    se crea funcion winner y score board para separar las responsabilidades de las funciones y poder reutilizar el codigo

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -17,41 +17,76 @@ choices = {
     's': emoji.emojize(':scissors:')
 }
 
+user_games = 0
+computer_games = 0
+tie_score = 0
+user_score = 0
+computer_score = 0
 
-def define_winner(choice1, choice2):
-  user_score = 0
-  computer_score = 0
-  user_games = 0
-  computer_games = 0
-  tie_score = 0
+def define_winner(choice1, choice2, game_mode):
+    
   if choice1 == choice2:
     print('It\'s a tie')
-    tie_score += 1
+    return 'tie'
+
   elif (
     choice1 == 'r' and choice2 == 's'or 
     choice1 == 'p' and choice2 == 'r' or
     choice1 == 's' and choice2 == 'p'):
-    print('You win!')
+    if game_mode == '1':
+      print('You win!')
+      return 'user'
+    else:
+      print('Player 1 wins!')
+      return 'user'	
+  else:
+    if game_mode == '2':
+      print('Player 2 wins!')
+      return 'computer'
+    else:
+      print('You lose!')
+      return 'computer'  
+
+def score_board (winner, game_mode):
+  global user_score, computer_score, tie_score, user_games, computer_games
+
+  if winner == 'tie':
+    tie_score += 1
+  elif winner == 'user':
     user_score += 1
   else:
-    print('You lose!')
     computer_score += 1
-  print(f'Your score: {user_score} and Computer score: {computer_score}')
-  if user_score == 2:
+  if game_mode == '1':
+    print(f'Your score: {user_score} and Computer score: {computer_score} and Ties: {tie_score}')
+  else:
+    print(f'Player1 score: {user_score} and Player2 score: {computer_score}')
+
+  if user_score == 2 and game_mode == '1':
     print('You won the game! Congrats!')
-    user_games += 1
+    user_games+= 1
     user_score = 0
     computer_score = 0
-    print(f'Your games: {user_games}, Computer games: {computer_games} and Ties: {tie_score}')
-  elif computer_score == 2:
-    print('You lost the game!')
-    computer_games += 1
-    computer_score = 0
-    user_score = 0
     print(f'Your games: {user_games}, Computer games: {computer_games} and Ties: {tie_score}')
   
-  if input('Continue playing?(Y/n):') == 'n':
-    print('GAME OVER \nThanks for playing!')
+  elif computer_score == 2 and game_mode== '1':
+    print('You lost the game!')
+    computer_games+= 1
+    computer_score = 0
+    user_score = 0
+    print(f'Your games: {user_games}, Computer games: {computer_games} and Ties: {tie_score}')
+
+  elif user_score == 2 and game_mode == '2':
+    print('Player1 won the game! Congrats!')
+    user_games+= 1
+    user_score = 0
+    computer_score = 0
+    print(f'Player1 games: {user_games}, Player2 games: {computer_games} and Ties: {tie_score}')
+  elif computer_score == 2 and game_mode == '2':
+    print('Player2 won the game!')
+    computer_games+= 1
+    computer_score = 0
+    user_score = 0
+    print(f'Player1 games: {user_games}, Player2 games: {computer_games} and Ties: {tie_score}')
 
 game_mode = input(
   "Welcome to Rock, Paper, Scissors!.\n\
@@ -71,7 +106,14 @@ if game_mode == '1':
 
     computer_choice = random.choice(list(choices.keys()))
     print(f'Your choice: {choices[user_choice]}  and Computer choice: {choices[computer_choice]}')
-    define_winner(user_choice, computer_choice)
+    winner = define_winner(user_choice, computer_choice, game_mode)
+
+    score_board(winner, game_mode)
+
+  
+    if input('Continue playing?(Y/n):') == 'n':
+      print('GAME OVER \nThanks for playing!')
+      break
     
 elif game_mode == '2':
   while True:
@@ -83,34 +125,11 @@ elif game_mode == '2':
       print('Invalid choice, please choose r, p, or s.')
       continue
     print(f'Player 1 choice: {choices[player1_choice]}  and Player 2 choice: {choices[player2_choice]}')
-    if player1_choice == player2_choice:
-      print('It\'s a tie')
-      tie_score += 1
-    elif player1_choice == 'r' and player2_choice == 's':
-      print('Player 1 wins!')
-      user_score += 1
-    elif player1_choice == 'p' and player2_choice == 'r':
-      print('Player 1 wins!')
-      user_score += 1
-    elif player1_choice == 's' and player2_choice == 'p':
-      print('Player 1 wins!')
-      user_score += 1
-    else:
-      print('Player 2 wins!')
-      computer_score += 1
-    print(f'Player 1 score: {user_score} and Player 2 score: {computer_score}')
-    if user_score == 2:
-      print('Player 1 won the game! Congrats!')
-      user_games += 1
-      user_score = 0
-      computer_score = 0
-      print(f'Player 1 games: {user_games}, Player 2 games: {computer_games} and Ties: {tie_score}')
-    elif computer_score == 2:
-      print('Player 2 won the game!')
-      computer_games += 1
-      computer_score = 0
-      user_score = 0
-      print(f'Player 1 games: {user_games}, Player 2 games: {computer_games} and Ties: {tie_score}')
+    
+    winner = define_winner(player1_choice, player2_choice, game_mode)
+
+    score_board(winner, game_mode)
+    
     if input('Continue playing?(Y/n):') == 'n':
       print('GAME OVER \nThanks for playing!')
       break
